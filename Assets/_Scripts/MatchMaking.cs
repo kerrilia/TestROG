f6c0b01c1b3694fb1bcb4b1ba893e5138f511e91
Xana <kerriolia@rambler.ru>
2018-11-27 17:59:20 +0300

    Коммит

---
+++ Assets/_Scripts/MatchMaking.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Timers;
+using System.Linq;
+
+public struct Player
+{
+    public int Id;
+    public int Power;
+}
+
+[Serializable]
+public struct MatchmakingOptions
+{
+    public int PartySize;
+    public int DefaultRange;
+    public int RangeIncreaseTime;
+    public int RangeIncrease;
+    public int MatchingTime;
+}
+
+public class PlayerMatch
+{
+    public Player PlayerData;
+    public int TimeWait;
+    public bool isMatch;
+}
+
+public class MatchMaking 
+{
+    public Action<Player[]> OnMatched;
+    public Action<Player> OnCanceled;
+    private static Timer aTimer;
+
+    public List<PlayerMatch> ListWaitingPlayers = new List<PlayerMatch>();
+
+    private MatchmakingOptions _options;
+
+    public MatchMaking(MatchmakingOptions options)
+    {
+        _options = options;
+        SetTimer();
+        aTimer.Start();
+    }
+
+    public void AddRequest(Player player)
+    {
+        var playerAdded = ListWaitingPlayers.Find(f => f.PlayerData.Id == player.Id);
+        if (playerAdded == null)
+        {
+            ListWaitingPlayers.Add(new PlayerMatch() { PlayerData = player, TimeWait = 0 });
+        }
+        else
+        {
+            ListWaitingPlayers.Remove(playerAdded);
+            throw new ArgumentException(string.Format("Player {0} already exist", player.Id));
+        }
+    }
+
+    public void CancelRequest(Player player)
+    {
+        var playerCancel = ListWaitingPlayers.Find(f => f.PlayerData.Id == player.Id);
+
+        if (playerCancel != null)
+        {
+            if(OnCanceled != null)
+                OnCanceled(player);
+
+            ListWaitingPlayers.Remove(playerCancel);
+        }
+        else
+        {
+            throw new ArgumentException(string.Format("Player {0} not found", player.Id));
+        }
+    }
+
+    public void Update(int time)
+    {
+        List<Party> parties = new List<Party>() { };
+
+        foreach (var item in ListWaitingPlayers)
+        {
+            if (item.TimeWait > _options.MatchingTime)
+                continue;
+
+            float intervalPowerPlayer = item.TimeWait * (_options.RangeIncrease + _options.DefaultRange) * 0.5f;
+            int powerPlayerMax = item.PlayerData.Power + Mathf.CeilToInt(intervalPowerPlayer);
+            int powerPlayerMin = item.PlayerData.Power - Mathf.FloorToInt(intervalPowerPlayer);
+
+            UpdateParties(parties, item);
+
+            if (!item.isMatch)
+                parties.Add(new Party() { players = new List<PlayerMatch>() { item }, MaxPower = powerPlayerMax, MinPower = powerPlayerMin });
+
+            item.TimeWait += time;
+        }
+
+        RemovePlayers();
+    }
+
+    private void UpdateParties(List<Party> parties, PlayerMatch item)
+    {
+        foreach (var party in parties)
+        {
+            if (party.players.Count < _options.PartySize)
+            {
+                if (item.PlayerData.Power <= party.MaxPower && item.PlayerData.Power >= party.MinPower)
+                    party.players.Add(item);
+
+                if (party.players.Count == _options.PartySize)
+                {
+                    foreach (var player in party.players)
+                        player.isMatch = true;
+
+                    if(OnMatched != null)
+                        OnMatched(party.players.Select(f => f.PlayerData).ToArray());
+                }
+            }
+        }
+
+        parties.RemoveAll(f => f.players.Count == _options.PartySize);
+    }
+
+    private void RemovePlayers()
+    {
+        List<PlayerMatch> canceledPlayers = new List<PlayerMatch>();
+        List<PlayerMatch> matchedPlayers = new List<PlayerMatch>();
+
+        foreach (var item in ListWaitingPlayers)
+        {
+            if (item.isMatch)
+            {
+                matchedPlayers.Add(item);
+            }
+            else if (item.TimeWait > _options.MatchingTime)
+            {
+                canceledPlayers.Add(item);
+            }
+        }
+
+        foreach (var m in matchedPlayers)
+        {
+            ListWaitingPlayers.Remove(m);
+        }
+
+        foreach (var c in canceledPlayers)
+        {
+            CancelRequest(c.PlayerData);
+        }
+    }
+
+    private void SetTimer()
+    {
+        aTimer = new Timer(_options.RangeIncreaseTime * 1000);//перевод в миллисекунды
+        aTimer.Elapsed += OnTimedEvent;
+        aTimer.AutoReset = true;
+        aTimer.Enabled = true;
+    }
+
+    private void OnTimedEvent(object sender, ElapsedEventArgs e)
+    {
+        Update(_options.RangeIncreaseTime);
+    }
+
+    public void Stop()
+    {
+        aTimer.Stop();
+        aTimer.Dispose();
+    }
+}
+
+public class Party
+{
+    public List<PlayerMatch> players;
+    public int MinPower;
+    public int MaxPower;
+}
---
+++ Assets/_Scripts/TestMatchMaking.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMatchMaking : MonoBehaviour
+{
+    public MatchmakingOptions Options;
+
+    public int MyId;
+    public int IdPlayerAdded;
+
+    private MatchMaking _matchMaking;
+
+    void Start()
+    {
+        _matchMaking = new MatchMaking(Options);
+        _matchMaking.OnMatched += OnMatchDone;
+    }
+
+    public void StartTest()
+    {
+        //на 100000 подвисает на несколько секунд
+        for (int i = 0; i < 10000; ++i)
+        {
+            _matchMaking.AddRequest(new Player() {Id = i, Power = Random.Range(1, 1000) });
+        }
+    }
+
+    public void CancelTest()
+    {
+        _matchMaking.CancelRequest(new Player() { Id = MyId, Power = 0 });
+    }
+
+    public void AddedPlayerTest()
+    {
+        _matchMaking.AddRequest(new Player() { Id = IdPlayerAdded, Power = 0 });
+    }
+
+    private void OnDestroy()
+    {
+        _matchMaking.Stop();
+        if(_matchMaking.OnMatched != null)
+            _matchMaking.OnMatched -= OnMatchDone;
+    }
+
+    private void OnMatchDone(Player[] players)
+    {
+        foreach (var player in players)
+        {
+            Debug.LogFormat("PARTY MATCH = {0}; {1}", player.Id, player.Power);
+        }
+    }
+}
